df3f251125a23734f55e00466fc0c8bcc38e0f79
Eduardo Ferreira <eduardoferreiraalmeida20@gmail.com>
2026-03-12 00:48:51 -0300

    feat: criado a segunda section com filmes da categoria de terror

--- src/js/apiMovies.js
+++ src/js/apiMovies.js
@@ -24,5 +24,51 @@ fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${apiKey}&langua
 
 });
 
+const container2 = document.getElementById("filmes-terror");
+
+fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=27&sort_by=popularity.desc&vote_count.gte=200&language=pt-BR`)
+
+.then(res => res.json())
+.then(data => {
+
+    const terror = data.results
+    terror.forEach(movie => {
+
+        const div = document.createElement("div")
+        const imagem = "https://image.tmdb.org/t/p/w500" + movie.poster_path;
+
+        div.innerHTML = `
+            <img src="${imagem}" width="350">
+            <p>${movie.title}</p>
+        `;
+
+        container2.appendChild(div);
+    })
+})
+
+
+const container3 = document.getElementById("filmes-animacao");
+
+fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=27&sort_by=popularity.desc&vote_count.gte=200&language=pt-BR`)
+
+.then(res => res.json())
+.then(data => {
+
+    const animacao = data.results
+    animacao.forEach(movie => {
+
+        const div = document.createElement("div")
+        const imagem = "https://image.tmdb.org/t/p/w500" + movie.poster_path;
+
+        div.innerHTML = `
+            <img src="${imagem}" width="350">
+            <p>${movie.title}</p>
+        `;
+
+        container3.appendChild(div);
+    })
+})
+
+
 
 
--- src/js/nextBack.js
+++ src/js/nextBack.js
@@ -8,3 +8,27 @@ next.addEventListener("click", () => {
 prev.addEventListener("click", () => {
         container.scrollLeft -= 380;
 });
+
+
+const next2 = document.getElementById("next-terror");
+const prev2 = document.getElementById("prev-terror");
+
+next2.addEventListener("click", () => {
+        container2.scrollLeft += 380;
+});
+
+prev2.addEventListener("click", () => {
+        container2.scrollLeft -= 380;
+});
+
+
+const next3 = document.getElementById("next-animacao");
+const prev3 = document.getElementById("prev-animacao");
+
+next3.addEventListener("click", () => {
+        container3.scrollLeft += 380;
+});
+
+prev3.addEventListener("click", () => {
+        container3.scrollLeft -= 380;
+});
